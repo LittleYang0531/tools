07cabb8dfece84a30e42c3822bae45281d9940fb
优秀的小杨同学 <63852815+LittleYang0531@users.noreply.github.com>
2022-02-28 20:41:51 +0800

    Add files via upload

---
+++ OMS/config.js
@@ -0,0 +1,5 @@
+const USER="LittleYang0531";
+const MAIL="littleyang0531@outlook.com";
+const TOKEN="ghp_l9G42lEY4L2X"+"hh9y5AhBySh2sNTTgi3rrbMK";
+const REPO="markdown-server";
+const BRANCH="main";
