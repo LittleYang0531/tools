f13a0faa81091f6aa0a17e25983b26f4a634f132
优秀的小杨同学 <63852815+LittleYang0531@users.noreply.github.com>
2022-10-20 19:19:57 +0800

    Update config.js

--- OMS/config.js
+++ OMS/config.js
@@ -3,3 +3,6 @@ const MAIL="littleyang0531@outlook.com";
 const TOKEN="ghp_l9G42lEY4L2X"+"hh9y5AhBySh2sNTTgi3rrbMK";
 const REPO="markdown-server";
 const BRANCH="main";
+
+const client_id="2baf98bc7a958d5b7fdf";
+const client_secret="e2ce36b369ae45fa076c1cbf01a6c822261531c4";
