d5c4f4e5cb359ae0f1466b5a3ea8952c05a2fc30
优秀的小杨同学 <63852815+LittleYang0531@users.noreply.github.com>
2022-10-20 21:41:06 +0800

    Update app.js

--- OMS/app.js
+++ OMS/app.js
@@ -294,13 +294,13 @@ class LoginGithub {
 	static CheckAuthor() {
 		var url="https://api.github.com/user";
         var header={"Authorization":'token '+$_COOKIE["access_token"]};
-        var loginname=SendAjax(url,"GET",null,false,null);
+        var loginname=SendAjax(url,"GET",null,false,null,header);
         if (loginname==null) {
             setCookie("access_token","",-100);
             location.replace(window.location.href.split('?')[0]);
         }loginname=loginname["login"];
         var header={"Authorization":'token '+TOKEN};
-        var adminname=SendAjax(url,"GET",null,false,null)["login"];
+        var adminname=SendAjax(url,"GET",null,false,null,header)["login"];
         return {login:loginname,admin:adminname};
 	}
 
